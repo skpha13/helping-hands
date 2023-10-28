cb4fdaaaf9dc251c40938b01c95b916f49673cd7
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-28 16:59:34 +0300

    feat: fetch for all posts

--- src/routes/feed.component.jsx
+++ src/routes/feed.component.jsx
@@ -1,59 +1,51 @@
 import FeedCard from "../components/feed_card.component";
 import logo1024 from "../assets/logo/logo-png-1024.png";
 import FilterOng from "../components/filters_ong.component";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Feed = () => {
-  const categories = [
-    {
-      name: "Carti",
-      quantity: 2,
-    },
-    {
-      name: "Haine",
-      quantity: 3,
-    },
-    {
-      name: "Mobila",
-      quantity: 4,
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const [arePostsLoaded, setPostsLoaded] = useState(false);
+
+  const getAllPosts = async () => {
+    try {
+      const response = await axios.get("https://localhost:7272/api/Post/posts");
+      return response.data.data;
+    } catch (error) {
+      if (error.response.data.status !== 200) {
+        console.log(error.response.data.message);
+      }
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const posts = await getAllPosts();
+      setPosts(posts);
+      setPostsLoaded(true);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <FilterOng />
 
       <div className="m-8">
-        {categories.map((category, index) => (
-          <FeedCard
-            key={index}
-            name="Un strop de fericire"
-            img={logo1024}
-            category={category}
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl ultricies nisl, quis ultricies nisl nisl quis nisl."
-            canHelp={true}
-          />
-        ))}
-
-        {/* <FeedCard
-          name="Un strop de fericire"
-          img={logo1024}
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl ultricies nisl, quis ultricies nisl nisl quis nisl."
-          categories={categories[0]}
-        />
-
-        <FeedCard
-          name="Un strop de fericire"
-          img={logo1024}
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl ultricies nisl, quis ultricies nisl nisl quis nisl."
-          categories={categories[1]}
-        />
-
-        <FeedCard
-          name="Un strop de fericire"
-          img={logo1024}
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultricies, nunc nisl ultricies nisl, quis ultricies nisl nisl quis nisl."
-          categories={categories[2]}
-        /> */}
+        {arePostsLoaded &&
+          posts.map((post, index) => (
+            <FeedCard
+              key={index}
+              name={post.title}
+              img={logo1024}
+              category={post.needs}
+              text={post.description}
+              canHelp={true}
+            />
+          ))}
       </div>
     </div>
   );
