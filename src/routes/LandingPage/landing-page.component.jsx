4d2c3bfefab2b0755ec593f5c4fe4f6e41a214a7
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-28 14:14:22 +0300

    fix: navbar links

--- src/routes/LandingPage/landing-page.component.jsx
+++ src/routes/LandingPage/landing-page.component.jsx
@@ -1,17 +1,31 @@
-import './landing-page.styles.css';
+import "./landing-page.styles.css";
 
 import { useNavigate } from "react-router-dom";
 const LandingPage = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="landing-container">
-            <div>
-            <h1 className="landing-title">Selecteaza cine esti: </h1>
-            <button className="landing-button" onClick={()=>{navigate('/utilizator')}}>Utilizator</button>
-            <button className="landing-button" onClick={()=>{navigate('/ong')}}>ONG</button>
-            </div>
-        </div>
-    );
-}
+  const navigate = useNavigate();
+  return (
+    <div className="landing-container">
+      <div>
+        <h1 className="landing-title">Selecteaza cine esti: </h1>
+        <button
+          className="landing-button"
+          onClick={() => {
+            navigate("/utilizator/feed");
+          }}
+        >
+          Utilizator
+        </button>
+        <button
+          className="landing-button"
+          onClick={() => {
+            navigate("/ong");
+          }}
+        >
+          ONG
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
