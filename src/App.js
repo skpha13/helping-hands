07968cc9cf5e4d9c6cf84752c89f448ebd509f44
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-28 10:56:05 +0300

    feat: button component + text sizes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPlus, faCircle } from '@fortawesome/free-solid-svg-icons'
 import Feed from './routes/feed.component';
 import LandingPage from './routes/LandingPage/landing-page.component';
+// import UserPage from './routes/UserPage/user-page.component';
+// import ONGPage from './routes/ONGPage/ong-page.component';
 
 library.add(fab, faPlus, faCircle);
 
@@ -14,11 +16,14 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />}>
-        {/* <Route path='utilizator' element={<UserPage/>}>
 
       </Route>
-      <Route path='ong' element={<ONGPage/>}></Route> */}
+
+      <Route path="/feed" element={<Feed />} />
+      {/* <Route path='utilizator' element={<UserPage />}>
+
       </Route>
+      <Route path='ong' element={<ONGPage />}></Route> */}
     </Routes>
   );
 }
