577e5a501ba4368b7db0415a2fe51498d2fe6198
anarosu <anarosu766@gmail.com>
2023-10-28 09:50:54 +0300

    feat: font setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+import ReactDOM from 'react-dom'
+import LandingPage from './routes/LandingPage/landing-page.component';
+import {Routes, Route} from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
 import logo from './logo-svg.svg';
 import './App.css';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route path="/" element={<LandingPage/>}></Route>
+   </Routes>
   );
 }
 
