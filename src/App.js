50faf2d8891c3005c0901c95e81f7e7d5133e5bc
anarosu <anarosu766@gmail.com>
2023-10-28 14:57:39 +0300

    ong routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPlus, faCircle, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import Feed from './routes/feed.component';
 import LandingPage from './routes/LandingPage/landing-page.component';
-import Dashboard from './routes/dashboard.component';
-import NavbarONG from './components/navbar_ong.component';
 import Navbar from './routes/navigation/nav.component';
+import Feed from './routes/feed.component';
 import UserMap from './routes/UserMap/user-map.component';
-// import ONGPage from './routes/ONGPage/ong-page.component';
+import OngProfile from './routes/OngProfile/ong-profile.component'
+import Dashboard from './routes/dashboard.component';
+
 import './App.css';
 
 library.add(fab, faPlus, faCircle, faMagnifyingGlass);
@@ -23,11 +23,11 @@ function App() {
         <Route path="harta" element={<UserMap />} />
       </Route>
 
-      <Route path='navbarong' element={<NavbarONG />} >
+      <Route path='/ong' element={<Navbar links={[{name: "Profil", path: "profil"}, {name: "Dashboard", path:"/dashboard"}]} />} >
+        <Route path='profile' element={<OngProfile />} />
         <Route path='dashboard' element={<Dashboard />} />
       </Route>
 
-      {/* <Route path='ong' element={<ONGPage />}></Route>  */}
     </Routes >
   );
 }
